511252e07e46e0b8d7857ed41e3c545592d64d17
ChingShing <ching_shing@sjtu.edu.cn>
2017-05-17 10:15:45 +0800

    exe 9

--- lab1/lib/printfmt.c
+++ lab1/lib/printfmt.c
@@ -114,6 +114,10 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		case '-':
 			padc = '-';
 			goto reswitch;
+
+		case '+':
+   			padc = '+';
+    		goto reswitch;
 			
 		// flag to pad with 0's instead of spaces
 		case '0':
